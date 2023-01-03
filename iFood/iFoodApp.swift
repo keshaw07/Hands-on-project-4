f73423e710a31c4c0759760d39cb6cf5c6340e81
Keshaw Ram <keshaw.ram@allegion.com>
2023-01-03 18:11:11 +0530

    Initial Commit

---
+++ iFood/iFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFoodApp.swift
+//  iFood
+//
+//  Created by Ram, Keshaw on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
